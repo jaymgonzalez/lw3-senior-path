56e3a83d5cccb5f84ef58b2b4612708cac0bd3c6
Jay M Gonzalez <jaimungon@gmail.com>
2022-11-14 10:17:55 +0100

    add tests

--- test/Attack.js
+++ test/Attack.js
@@ -71,4 +71,31 @@ describe('Attack', function () {
     expect(ethers.utils.parseBytes32String(slot0Bytes)).to.equal('test')
     expect(ethers.utils.parseBytes32String(slot1Bytes)).to.equal('password')
   })
+  it('Should change the owner of the Good contract', async function () {
+    // Deploy the helper contract
+    const helperContract = await ethers.getContractFactory('Helper')
+    const _helperContract = await helperContract.deploy()
+    await _helperContract.deployed()
+    console.log("Helper Contract's Address:", _helperContract.address)
+
+    // Deploy the good contract
+    const goodContract = await ethers.getContractFactory('Good')
+    const _goodContract = await goodContract.deploy(_helperContract.address)
+    await _goodContract.deployed()
+    console.log("Good Contract's Address:", _goodContract.address)
+
+    // Deploy the Attack contract
+    const attackContract = await ethers.getContractFactory('Attack')
+    const _attackContract = await attackContract.deploy(_goodContract.address)
+    await _attackContract.deployed()
+    console.log("Attack Contract's Address", _attackContract.address)
+
+    // Now lets attack the good contract
+
+    // Start the attack
+    let tx = await _attackContract.attack()
+    await tx.wait()
+
+    expect(await _goodContract.owner()).to.equal(_attackContract.address)
+  })
 })
